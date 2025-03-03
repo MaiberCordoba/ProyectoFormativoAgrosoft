39b246a43691b37eeec63a0e4cfedbe97213479a
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-02 19:37:25 -0500

    agrengando la ruta del websocket de actividades a la ruta principal

--- backend/AgroSis/routing.py
+++ backend/AgroSis/routing.py
@@ -1,8 +1,9 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
+from apps.finanzas.websocket.routing import websocket_urlpatterns as actividades_routing
 
 #routers de los webSockets
-websocket_urlpatterns = 
+websocket_urlpatterns = actividades_routing
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter(websocket_urlpatterns),
