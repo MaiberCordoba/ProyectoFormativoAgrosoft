b3557e8eea0e68b7ad77c70d95facb550256d4a9
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-06 18:02:00 -0500

    creando el componente de tabla y ajustando el layaout principal para manejar rutas

--- frontend/AgroSoft_f/src/App.tsx
+++ frontend/AgroSoft_f/src/App.tsx
@@ -1,19 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
 import { UsersPage } from "./modules/test/pages/userPage";
 import Principal from "@/layouts/principal";
+import { TablePage } from "./modules/test/pages/tablaPage";
+ // Nueva página
 
 const queryClient = new QueryClient(); // Crea la instancia de QueryClient
 
 function App() {
   return (
-
     <QueryClientProvider client={queryClient}>
       <Routes>
         <Route element={<Principal />}>
           <Route path="/" element={<UsersPage />} />
+          <Route path="/tabla" element={<TablePage />} />
+          
         </Route>
       </Routes>
     </QueryClientProvider>
--- frontend/AgroSoft_f/src/layouts/principal.tsx
+++ frontend/AgroSoft_f/src/layouts/principal.tsx
@@ -1,17 +1,19 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { Outlet } from "react-router-dom";  // Importa Outlet
 
-const Principal = ({ children }: { children: React.ReactNode }) => {
+const Principal = () => {
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex flex-col flex-1">
         <Navbar />
-        <main className="p-6">{children}</main>
+        <main className="p-6">
+          <Outlet />  {/* Aquí se renderizarán las rutas hijas */}
+        </main>
       </div>
     </div>
   );
 };
 
 export default Principal;
-
