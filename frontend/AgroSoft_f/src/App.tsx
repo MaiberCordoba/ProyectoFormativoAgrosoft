8541bf57677ad08b5597238356974f83d4463a5a
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-07 01:14:10 -0500

    creando el login y el manejo de autenticacion

--- frontend/AgroSoft_f/src/App.tsx
+++ frontend/AgroSoft_f/src/App.tsx
@@ -4,21 +4,27 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { UsersPage } from "./modules/test/pages/userPage";
 import Principal from "@/layouts/principal";
 import { Inicio } from "./pages/Inicio";
+import { AuthProvider } from "@/context/AuthContext";
+import ProtectedRoute from "@/routes/ProtectedRoute";
+import Login from "@/pages/Login";
 
- // Nueva página
-
-const queryClient = new QueryClient(); // Crea la instancia de QueryClient
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route element={<Principal />}>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/usuarios" element={<UsersPage />} />
-        </Route>
-      </Routes>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route element={<Principal />}>
+            <Route path="/" element={<Inicio />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/usuarios" element={<UsersPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
 
