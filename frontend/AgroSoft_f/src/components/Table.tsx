2d49acaf383688cd80691fbdc000c95d818ba3a7
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-06 19:49:24 -0500

    modificando el componente table para agregar acciones

--- frontend/AgroSoft_f/src/components/Table.tsx
+++ frontend/AgroSoft_f/src/components/Table.tsx
@@ -1,31 +1,32 @@
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@heroui/react";
 
 interface TableComponentProps<T> {
-  columns: { key: keyof T; label: string }[]; // Definimos las columnas de la tabla dinámicamente
-  data: T[]; // Recibimos cualquier tipo de datos
+  columns: { key: keyof T | "acciones"; label: string }[];
+  data: T[];
+  renderActions?: (row: T) => JSX.Element; // Nueva prop para botones en la tabla
 }
 
-const TableComponent = <T,>({ columns, data }: TableComponentProps<T>) => {
+export default function TableComponent<T>({ columns, data, renderActions }: TableComponentProps<T>) {
   return (
-    <Table aria-label="Tabla dinámica">
+    <Table aria-label="User Data Table">
       <TableHeader>
-        {columns.map(column => (
+        {columns.map((column) => (
           <TableColumn key={column.key as string}>{column.label}</TableColumn>
         ))}
       </TableHeader>
       <TableBody>
         {data.map((row, rowIndex) => (
           <TableRow key={rowIndex}>
-            {columns.map(column => (
-              <TableCell key={`${rowIndex}-${column.key as string}`}>
-                {String(row[column.key])}
-              </TableCell>
+            {columns.map((column) => (
+             <TableCell key={column.key as string}>
+             {column.key === "acciones" && renderActions
+               ? renderActions(row)
+               : String(row[column.key as keyof T])} {/* Convertimos a string */}
+           </TableCell>
             ))}
           </TableRow>
         ))}
       </TableBody>
     </Table>
   );
-};
-
-export default TableComponent;
+}
