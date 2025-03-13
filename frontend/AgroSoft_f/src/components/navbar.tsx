f65a51b38bf8eef11319972ea08bcf4b983082c7
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-13 15:48:45 -0500

    modificando el navbar

--- frontend/AgroSoft_f/src/components/navbar.tsx
+++ frontend/AgroSoft_f/src/components/navbar.tsx
@@ -7,7 +7,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ toggleSidebar }) => {
   return (
-    <nav className="flex items-center justify-between bg-green-700 p-3 text-white">
+    <nav className="flex items-center justify-between bg-green-700 p-2 text-white">
       <div className="flex items-center gap-4">
         {/* Botón para ocultar/mostrar Sidebar */}
         <button onClick={toggleSidebar} className="p-2 rounded-full hover:bg-green-800">
@@ -15,16 +15,16 @@ const Navbar: React.FC<NavbarProps> = ({ toggleSidebar }) => {
         </button>
 
         <input
-          type="text"
+          type="text-sm"
           placeholder="Buscar..."
-          className="px-4 py-2 rounded-full text-black focus:outline-none"
+          className="px-8 py-1 rounded-full text-black focus:outline-none"
         />
         <Search className="text-white cursor-pointer" />
       </div>
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-4">
         <Bell className="cursor-pointer" />
         <User className="cursor-pointer" />
-        <Link to="/login" className="flex items-center gap-2 cursor-pointer hover:underline">
+        <Link to="/login" className="flex items-center gap-1 cursor-pointer hover:underline">
           <LogOut /> Cerrar sesión
         </Link>
       </div>
