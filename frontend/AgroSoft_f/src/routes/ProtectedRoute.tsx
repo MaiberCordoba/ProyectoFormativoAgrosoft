eca65df5fb881722648dd7228b2b7932c331b3d8
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-13 14:35:37 -0500

    pemitiendo que cualquier usuario registrado ingrese a la pagina de inicio

--- frontend/AgroSoft_f/src/routes/ProtectedRoute.tsx
+++ frontend/AgroSoft_f/src/routes/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import {useAuth} from "../hooks/UseAuth";
 
 const ProtectedRoute = () => {
   const { token } = useAuth();
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return token ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;
