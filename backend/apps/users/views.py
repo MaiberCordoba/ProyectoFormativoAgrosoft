8ca69a494aef994aa0507a667c6f082f6386a4a8
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-17 16:27:30 -0500

    arreglando para registro de usuarios

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,10 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from .models import Usuario
 from .serializers import UsuarioSerializer
-# Create your views here.
 
 class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-    permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.request.method == "POST":  # Permitir acceso sin autenticación solo para POST
+            return [AllowAny()]
+        return [IsAdminUser()]  # Para los demás métodos, se requiere ser administrador
