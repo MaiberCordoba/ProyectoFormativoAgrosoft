8b8f9eaa14f9e3095f1f489f8400ba7704468e90
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-17 21:02:04 -0500

    montando la pagina afecciones

--- frontend/AgroSoft_f/src/App.tsx
+++ frontend/AgroSoft_f/src/App.tsx
@@ -9,6 +9,7 @@ import ProtectedRoute from "@/routes/ProtectedRoute";
 import Login from "@/pages/Login";
 import UserRegister from "./modules/Users/pages/registrarUsuario";
 import { useAuth } from '@/hooks/UseAuth'; // Usa el hook aquí
+import { AfeccionesList } from "./modules/Sanidad/components/listAfecciones";
 
 const queryClient = new QueryClient();
 
@@ -33,6 +34,7 @@ function App() {
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Inicio />} />
             <Route path="/usuarios" element={<UsersPage />} />
+            <Route path="/afectaciones" element={<AfeccionesList />} />
           </Route>
         </Route>
       </Routes>
