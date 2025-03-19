d923f48f020bb5ecf5a9c1472d6641ae87dcc607
Mamuel17 <rubyesperanza596@gmail.com>
2025-03-19 18:32:36 -0500

    cambios en los modelos de especie y semillero

--- backend/apps/trazabilidad/api/models/SemillerosModel.py
+++ backend/apps/trazabilidad/api/models/SemillerosModel.py
@@ -2,9 +2,9 @@ from django.db import models
 from .EspeciesModel import Especies
 
 class Semilleros(models.Model):
-    fk_Especie = models.ForeignKey(Especies,on_delete=models.SET_NULL,null=True)
+    fk_especie = models.ForeignKey(Especies,on_delete=models.SET_NULL,null=True)
     unidades = models.IntegerField()
-    fechaSiembra = models.DateField()
-    fechaEstimada = models.DateField()
+    fechasiembra = models.DateField()
+    fechaestimada = models.DateField()
     def __str__(self):
         return ('Semillero de especie:'+str(self.fk_Especie))
