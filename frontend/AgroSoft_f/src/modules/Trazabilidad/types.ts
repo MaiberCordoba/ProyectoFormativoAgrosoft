1701f4c2e5ed88a0aae5bb664a2aeb5dcc4e257e
Mamuel17 <rubyesperanza596@gmail.com>
2025-03-19 14:49:46 -0500

    Tipado para semillero

--- frontend/AgroSoft_f/src/modules/Trazabilidad/types.ts
+++ frontend/AgroSoft_f/src/modules/Trazabilidad/types.ts
@@ -3,7 +3,7 @@ export interface Semilleros {
   fk_Especie: number;
   unidades: string;
   fechaSiembra: string;
-  fechaEstimada: string; // Puedes usar Date si lo parseas antes de usarlo>>>
+  fechaEstimada: string; // Puedes usar Date si lo parseas antes de usarlo
 }
 
 export interface Cultivos {
