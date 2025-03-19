395de591863310d76bc754433fa74ebc2202b6cc
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-18 23:02:21 -0500

    permitiendo q admin mire un componente  y user normal otra en la misma url

--- frontend/AgroSoft_f/src/modules/Users/pages/userPage.tsx
+++ frontend/AgroSoft_f/src/modules/Users/pages/userPage.tsx
@@ -1,10 +1,13 @@
 import { UserList } from "../components/listarUsuarios";
+import { Afecciones } from "@/modules/Sanidad/Pages/afecciones";
 
 export function UsersPage() {
+  const user = JSON.parse(localStorage.getItem("user") || "{}"); // Obtener el usuario guardado
+
   return (
     <div>
       <h1>Usuarios</h1>
-      <UserList />
+      {user.admin ? <UserList /> : <Afecciones />}
     </div>
   );
 }
