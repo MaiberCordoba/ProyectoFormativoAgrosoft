d314279a108967de7eb13ba5253efb3c8b57992a
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-03-20 21:05:22 -0500

    Agregando boton para registrar una cosecha

--- frontend/AgroSoft_f/src/modules/Finanzas/pages/pageCosechas.tsx
+++ frontend/AgroSoft_f/src/modules/Finanzas/pages/pageCosechas.tsx
@@ -1,8 +1,14 @@
 import { CosechasList } from "../components/listarCosechas";
+import { Button, Link } from "@heroui/react";
 
 export function Cosechas(){
     return(
         <div>
+            <Link href="/registro-cosecha">
+            <Button color="primary" size="sm">
+                Registrar Cosecha
+            </Button>
+            </Link>
             <CosechasList/>
         </div>
     )
