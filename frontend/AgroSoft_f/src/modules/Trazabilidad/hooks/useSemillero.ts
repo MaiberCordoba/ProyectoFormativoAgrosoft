f8e56192d57db77da2f134a6d2155c4bb632aac7
Mamuel17 <rubyesperanza596@gmail.com>
2025-03-21 02:09:06 -0500

    se creo un solo archivo hook para todo y se elimino los demas

--- frontend/AgroSoft_f/src/modules/Trazabilidad/hooks/useSemillero.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getSemilleros } from "../api/semilleroApi";
-
-export const useSemilleros = () => {
-  return useQuery({
-    queryKey: ["semilleros"], 
-    queryFn: getSemilleros,
-  });
-};
