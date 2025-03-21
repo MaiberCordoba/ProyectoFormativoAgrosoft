7000efa9bbc4349223cd3b3a1c54229c71323c8d
Mamuel17 <rubyesperanza596@gmail.com>
2025-03-20 20:45:03 -0500

    correccion de errores en listar y actualizar semillero

--- frontend/AgroSoft_f/src/modules/Trazabilidad/types.ts
+++ frontend/AgroSoft_f/src/modules/Trazabilidad/types.ts
@@ -5,3 +5,13 @@ export interface Semillero {
   fechaSiembra: string;
   fechaEstimada: string; // Puedes usar Date si lo parseas antes de usarlo
 }
+
+export interface Cultivos {
+  id: number;
+  nombre: string;
+  fk_especie: number;
+  unidades: string;
+  fechaSiembra: string;
+  activo: boolean; // Para indicar si el cultivo está activo o no
+}
+
