1ad6c9b95a4777077f5fab1c7d2c08be4f8d98df
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-03-20 20:58:37 -0500

    Consumo al crud completo para cosechas

--- frontend/AgroSoft_f/src/modules/Finanzas/api/cosechasApi.ts
+++ frontend/AgroSoft_f/src/modules/Finanzas/api/cosechasApi.ts
@@ -2,16 +2,20 @@ import apiClient from "@/api/apiClient";
 import { Cosechas } from "../types";
 
 export const getCosechas = async (): Promise<Cosechas[]> => {
-    const response = await apiClient.get("cosechas/")
-    return response.data
+  const response = await apiClient.get("cosechas/")
+  return response.data
 }
 
-export const postCosechas = async (cosechasData : Partial<Cosechas>): Promise<Cosechas> => {
-    const response = await apiClient.post("cosechas/",cosechasData)
-    return response.data
-}
+export const postCosecha = async (CosechasData: Partial<Cosechas>): Promise<Cosechas> => {
+  const response = await apiClient.post("cosechas/", CosechasData);
+  return response.data;
+};
 
 export const putCosechas = async (id: number, data: Cosechas): Promise<Cosechas> => {
-    const response = await apiClient.put(`cosechas/${id}`, data);
-    return response.data;
+  const response = await apiClient.put(`cosechas/${id}/`, data);
+  return response.data;
+}
+
+export const deleteCosechas = async (id: number): Promise<void> => {
+  await apiClient.delete(`cosechas/${id}/`)
 }
