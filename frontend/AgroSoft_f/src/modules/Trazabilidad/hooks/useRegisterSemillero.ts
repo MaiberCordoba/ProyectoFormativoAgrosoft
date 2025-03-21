d4158635c4c845f4fdb3883e2f3388d0430c1cd5
Mamuel17 <rubyesperanza596@gmail.com>
2025-03-21 02:09:06 -0500

    se creo un solo archivo hook para todo y se elimino los demas

--- frontend/AgroSoft_f/src/modules/Trazabilidad/hooks/useRegisterSemillero.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { registerSemillero } from "@/modules/Trazabilidad/api/semilleroApi";
-import { Semillero } from "../types";
-
-export const useRegisterSemillero = () => {
-  return useMutation<Semillero, Error, Partial<Semillero>>({
-    mutationFn: registerSemillero,
-  });
-};
