8c6bb8742fccfda9a0231d07aeb3505c16b0a927
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-03-20 21:09:28 -0500

    modificacion del hook cosechas, para realizar el post

--- frontend/AgroSoft_f/src/modules/Finanzas/hooks/useCosechas.ts
+++ frontend/AgroSoft_f/src/modules/Finanzas/hooks/useCosechas.ts
@@ -1,9 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCosechas } from "../api/cosechasApi";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { getCosechas, postCosecha} from "../api/cosechasApi";
+import { Cosechas } from "../types";
+
 
 export const useCosechas = () => {
   return useQuery({
     queryKey: ["cosechas"], 
     queryFn: getCosechas,
+  });
+};
+
+export const usePostCosechas = () => {
+  return useMutation<Cosechas, Error, Partial<Cosechas>>({
+    mutationFn: postCosecha,
   });
 };
