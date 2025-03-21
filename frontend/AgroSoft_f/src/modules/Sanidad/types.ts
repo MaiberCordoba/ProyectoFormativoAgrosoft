f56203d8673916aa0e04bcc5a3c6ad9050c786e3
MaiberCordoba <cordobamaiber@gmail.com>
2025-03-20 23:04:10 -0500

    listar tipos afectaciones

--- frontend/AgroSoft_f/src/modules/Sanidad/types.ts
+++ frontend/AgroSoft_f/src/modules/Sanidad/types.ts
@@ -1,7 +1,13 @@
+export interface TiposAfecciones{
+    id: number;
+    nombre: string;
+    descripcion: Text;
+}
+
 export interface Afecciones{
     id: number;
     nombre: string;
     descripcion: string;
     img: string;
-    tipoPlaga: string;
+    tipoPlaga: TiposAfecciones;
 }
