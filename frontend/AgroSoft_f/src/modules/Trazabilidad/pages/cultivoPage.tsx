fc4a2e241f19e64e2072984c372bbb9564c8eb83
Mamuel17 <rubyesperanza596@gmail.com>
2025-03-20 20:44:15 -0500

    modulo de cultivos creado

---
+++ frontend/AgroSoft_f/src/modules/Trazabilidad/pages/cultivoPage.tsx
@@ -0,0 +1,10 @@
+import { CultivoList } from "../components/listarCultivos";
+
+export function CultivoPage() {
+  return (
+    <div>
+      <h1>Cultivos</h1>
+      <CultivoList />
+    </div>
+  );
+}
