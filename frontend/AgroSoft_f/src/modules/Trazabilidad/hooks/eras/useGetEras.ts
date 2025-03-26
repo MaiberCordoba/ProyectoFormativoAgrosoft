203766191a85a2ea9fb86784a34255c7210c08f6
Mamuel17 <rubyesperanza596@gmail.com>
2025-03-26 18:05:10 -0500

    correcion del tipo de archivo en el hook de get para cada tabla (.ts) y corrigiendo la importacion del get desde la api

--- frontend/AgroSoft_f/src/modules/Trazabilidad/hooks/eras/useGetEras.ts
+++ frontend/AgroSoft_f/src/modules/Trazabilidad/hooks/eras/useGetEras.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getEras } from "../../api/ErasApi";
+import { getEras } from "../../api/erasApi";
 import { Eras } from "../../types";
 
 export const useGetEras = () => {
--- frontend/AgroSoft_f/src/modules/Trazabilidad/hooks/plantaciones/useGetPlantaciones.ts
+++ frontend/AgroSoft_f/src/modules/Trazabilidad/hooks/plantaciones/useGetPlantaciones.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getPlantaciones } from "../../api/PlantacionesApi";
+import { getPlantaciones } from "../../api/plantacionesApi";
 import { Plantaciones } from "../../types";
 
 export const useGetPlantaciones = () => {
