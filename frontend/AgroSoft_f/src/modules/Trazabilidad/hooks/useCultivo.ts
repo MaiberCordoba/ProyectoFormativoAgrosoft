cdc12e7fb01bd5274995b57a95cd6d48cc3e29b8
Mamuel17 <rubyesperanza596@gmail.com>
2025-03-26 15:35:48 -0500

    eliminando archivos para crear el modulo trazabilidad nuevamente

--- frontend/AgroSoft_f/src/modules/Trazabilidad/hooks/useCultivo.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCultivos } from "../api/cultivoApi";
-
-export const useCultivos = () => {
-  return useQuery({
-    queryKey: ["cultivos"], 
-    queryFn: getCultivos,
-  });
-};
--- frontend/AgroSoft_f/src/modules/Trazabilidad/hooks/useRegisterCultivo.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { registerCultivos } from "@/modules/Trazabilidad/api/cultivoApi";
-import { Cultivos } from "../types";
-
-export const useRegisterCultivo = () => {
-  return useMutation<Cultivos, Error, Partial<Cultivos>>({
-    mutationFn: registerCultivos,
-  });
-};
