c1bff0854cc1015a6dc2630ecaba9b2ab33927ff
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-03-27 16:14:37 -0500

    Correcion en el endpoint de tipos-desechos para editar y eliminar

--- frontend/AgroSoft_f/src/modules/Finanzas/api/tiposDesechosApi.ts
+++ frontend/AgroSoft_f/src/modules/Finanzas/api/tiposDesechosApi.ts
@@ -12,11 +12,11 @@ export const postTiposDesechos = async (TiposDesechosData: Partial<TiposDesechos
 };
 
 export const patchTiposDesechos = async ( id: number, data: Partial<TiposDesechos>): Promise<TiposDesechos> => {
-    const response = await apiClient.patch<TiposDesechos>(`Tipos-desechos/${id}/`, data);
+    const response = await apiClient.patch<TiposDesechos>(`tipos-desechos/${id}/`, data);
     return response.data;
   };
 
   export const deleteTiposDesechos = async (id: number): Promise<TiposDesechos> => {
-    const response = await apiClient.delete<TiposDesechos>(`Tipos-desechos/${id}/`);
+    const response = await apiClient.delete<TiposDesechos>(`tipos-desechos/${id}/`);
     return response.data
 }
