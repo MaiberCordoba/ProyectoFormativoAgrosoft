daea83a461e1828658f9932d8d4358750241c874
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-03-27 14:17:16 -0500

    Agrego los modelos de las fk para luego ser mas descriptivo

--- frontend/AgroSoft_f/src/modules/Finanzas/types.ts
+++ frontend/AgroSoft_f/src/modules/Finanzas/types.ts
@@ -1,9 +1,13 @@
 //Creacion de los tipos de datos
+import { Cultivos } from "../Trazabilidad/types"
+import { User } from "../Users/types"
 
 export interface Actividades {
     id : number,
     fk_Cultivo : number,
+    cultivo: Cultivos,
     fk_Usuario : number,
+    usuario : User,
     titulo : string,
     descripcion : string,
     fecha : string,
@@ -13,12 +17,15 @@ export interface Actividades {
 export interface UsosHerramientas {
     id : number,
     fk_Herramientas : number,
+    herramienta: Herramientas,
     fk_Actividad : number
+    actividad : Actividades
 }
 
 export interface Herramientas {
     id : number,
     fk_Lote : number,
+    lote : Lotes ,
     nombre : string,
     descripcion : string,
     unidades : number
@@ -27,13 +34,16 @@ export interface Herramientas {
 export interface UsosProductos {
     id : number,
     fk_Insumo : number,
+    insumo : Insumos ,
     fk_Actividad : number,
+    actividad : Actividades,
     cantidadProducto : number
 }
 
 export interface Cosechas {
     id : number,
     fk_Cultivo : number,
+    cultivo : Cultivos,
     unidades : number,
     fecha : string
 }
@@ -41,13 +51,16 @@ export interface Cosechas {
 export interface Ventas {
     id : number,
     fk_Cosecha : number,
+    cosecha : Cosechas,
     precioUnitario : number,
     fecha : string
 }
 export interface Desechos {
     id : number,
     fk_Cultivo : number,
+    cultivo : Cultivos,
     fk_TipoDesecho : number,
+    tipoDesecho : TiposDesechos,
     nombre : string,
     descripcion : string
 }
