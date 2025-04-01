2995c418b94d530993d32600ba1f200abbfb0620
maiberCordoba <cordobamaiber@gmail.com>
2025-03-31 22:08:20 -0500

    creando manual de usuario agrosoft login y home

---
+++ manual_Usuario_Agrosoft/astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'AgroSof',
+			social: {
+				github: 'https://github.com/MaiberCordoba/ProyectoFormativoAgrosoft',
+			},
+			sidebar: [
+				{
+					label: 'Primeros pasos',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Pantalla de ingreso', slug: 'primerospasos/login' },
+						{ label: 'Pagina de Inicio', slug: 'primerospasos/home' },
+					],
+				},
+				
+			],
+		}),
+	],
+});
