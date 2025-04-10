97b0cb2359d247e47206db0024b45936133a038f
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-04-10 16:48:04 -0500

    Archivo innecesario

--- backend/apps/finanzas/api/routers/routerUsosProductos.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from apps.finanzas.api.views.viewUsosProductos import ViewUsosProductos
-
-routerUsosProductos = DefaultRouter()
-routerUsosProductos.register(prefix="usos-productos",viewset=ViewUsosProductos,basename="usos-productos")
