a0ee8e621b6008ae764c34d5606dee44a6110da7
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-04-10 16:51:45 -0500

    Modificacion en el modelo ventas agregando campos necesarios

--- backend/apps/finanzas/api/models/ventas.py
+++ backend/apps/finanzas/api/models/ventas.py
@@ -1,9 +1,19 @@
 from django.db import models
 from apps.finanzas.api.models.cosechas import Cosechas
+# from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
 
 class Ventas(models.Model):
     fk_Cosecha = models.ForeignKey(Cosechas, on_delete=models.SET_NULL, null=True)
     precioUnitario = models.IntegerField()
     fecha = models.DateField(auto_now=False)
+    # fk_UnidadMedida = models.ForeignKey(UnidadesMedida, on_delete=models.SET_NULL, null=True)
+    cantidad = models.IntegerField()
+    valorTotal = models.IntegerField(blank=True, null=True) #blank = no es obligatorio en Formulario #null = puede estar vacio en la bd antes de guardarse
+
+    def save(self, *args, **kwargs): #args y kwargs permite capturar cualquier argumento que que django le pase al metodo
+        # Calcula el valor total antes de guardar
+        self.valorTotal = self.precioUnitario * self.cantidad
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return self.fecha
+        return str(self.fecha)
