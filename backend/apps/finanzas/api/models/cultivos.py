231f11a55615535a189c531026c68c6eebb6c0dd
Mamuel17 <rubyesperanza596@gmail.com>
2025-04-10 15:38:49 -0500

    añadiendo una nueva llave foranea al modelo de cultivos para semillero

--- backend/apps/finanzas/api/models/cultivos.py
+++ backend/apps/finanzas/api/models/cultivos.py
@@ -1,7 +1,11 @@
 from django.db import models
+from apps.trazabilidad.api.models.EspeciesModel import Especies
 from apps.trazabilidad.api.models.SemillerosModel import Semilleros
+
 class Cultivos(models.Model):
-    fk_Semillero = models.ForeignKey(Semilleros,on_delete=models.SET_NULL, null=True)
+    fk_Especie = models.ForeignKey(Especies,on_delete=models.SET_NULL, null=True)
+    fk_semillero = models.ForeignKey(Semilleros,on_delete=models.SET_NULL, null=True)
+    nombre = models.CharField(max_length=30)
     unidades = models.IntegerField()
     activo = models.BooleanField(default=False, null=False)
     fechaSiembra = models.DateField(auto_now=False)
