42f51ecc42279e4b2988c438cd943e5c132b2477
maiberCordoba <cordobamaiber@gmail.com>
2025-04-10 19:19:10 -0500

    conectando los modelos por medio de fk en tiempoActividadControl que apunta a actividad, control, salario y unidad tiempo

--- backend/apps/finanzas/api/models/tiempoActividadControl.py
+++ backend/apps/finanzas/api/models/tiempoActividadControl.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from finanzas.api.models.unidadesTiempo import UnidadesTiempo
+from finanzas.api.models.actividades import Actividades
+from sanidad.api.models.controlesModel import Controles
+from finanzas.api.models.salarios import Salarios
+
 
 class TiempoActividadControl(models.Model):
     tiempo = models.IntegerField(verbose_name="Cantidad de tiempo")  # Ej: 2
     valorTotal = models.FloatField(verbose_name="Valor total", editable=False) 
-    fk_unidadTiempo = models.ForeignKey('UnidadesTiempo', on_delete=models.SET_NULL, null=True)  # Ej: hora (equivale a 60 min)
-    fk_actividad = models.ForeignKey('Actividad', on_delete=models.SET_NULL,null=True)
-    fk_control = models.ForeignKey('Control', on_delete=models.SET_NULL,null=True)
-    fk_salario = models.ForeignKey('Salarios', on_delete=models.SET_NULL,null=True)  # Contiene el campo monto_minutos
+    fk_unidadTiempo = models.ForeignKey(UnidadesTiempo, on_delete=models.SET_NULL, null=True)  # Ej: hora (equivale a 60 min)
+    fk_actividad = models.ForeignKey(Actividades, on_delete=models.SET_NULL,null=True)
+    fk_control = models.ForeignKey(Controles, on_delete=models.SET_NULL,null=True)
+    fk_salario = models.ForeignKey(Salarios, on_delete=models.SET_NULL,null=True)  # Contiene el campo monto_minutos
 
     def clean(self):
         if self.fk_actividad and self.fk_control:
@@ -17,7 +22,7 @@ class TiempoActividadControl(models.Model):
 
     def save(self, *args, **kwargs):
         # 1. Convertir a minutos
-        minutos_equivalentes = self.tiempo * self.fk_unidadTiempo.equivalencia_minutos
+        minutos_equivalentes = self.tiempo * self.fk_unidadTiempo.equivalenciaMinutos
 
         # 2. Calcular valor total
         self.valorTotal = minutos_equivalentes * self.fk_salario.monto_minutos
