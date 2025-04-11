cd07bda79182dae514f41aaba6618ffccc63632b
maiberCordoba <cordobamaiber@gmail.com>
2025-04-10 22:37:01 -0500

    logica de stock agregada a cocechas y ventas

--- backend/apps/finanzas/api/models/cosechas.py
+++ backend/apps/finanzas/api/models/cosechas.py
@@ -1,11 +1,24 @@
 from django.db import models
 from apps.finanzas.api.models.cultivos import Cultivos
-#from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
+from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
+
 class Cosechas(models.Model):
-    fk_Cultivo = models.ForeignKey(Cultivos, on_delete = models.SET_NULL, null= True)
-    #fk_UnidadMedida = models.ForeignKey(UnidadesMedida, on_delete = models.SET_NULL, null= True)
+    fk_Cultivo = models.ForeignKey(Cultivos, on_delete=models.SET_NULL, null=True)
+    fk_UnidadMedida = models.ForeignKey(UnidadesMedida, on_delete=models.SET_NULL, null=True)
     cantidad = models.IntegerField()
+    cantidadTotal = models.FloatField(null=True, blank=True)
+    cantidadDisponible = models.FloatField(default=0)
     fecha = models.DateField(auto_now=False)
     precioReferencial = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        if self.fk_UnidadMedida:
+            self.cantidadTotal = self.cantidad * self.fk_UnidadMedida.equivalenciabase
+
+            if self.cantidadDisponible == 0 or self._state.adding:
+                self.cantidadDisponible = self.cantidadTotal
+
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return self.fecha
+        return str(self.fecha)
--- backend/apps/finanzas/api/models/ventas.py
+++ backend/apps/finanzas/api/models/ventas.py
@@ -1,18 +1,25 @@
 from django.db import models
 from apps.finanzas.api.models.cosechas import Cosechas
-# from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
+from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
 
 class Ventas(models.Model):
     fk_Cosecha = models.ForeignKey(Cosechas, on_delete=models.SET_NULL, null=True)
     precioUnitario = models.IntegerField()
     fecha = models.DateField(auto_now=False)
-    # fk_UnidadMedida = models.ForeignKey(UnidadesMedida, on_delete=models.SET_NULL, null=True)
+    fk_UnidadMedida = models.ForeignKey(UnidadesMedida, on_delete=models.SET_NULL, null=True)
     cantidad = models.IntegerField()
-    valorTotal = models.IntegerField(blank=True, null=True) #blank = no es obligatorio en Formulario #null = puede estar vacio en la bd antes de guardarse
+    valorTotal = models.IntegerField(blank=True, null=True)
 
-    def save(self, *args, **kwargs): #args y kwargs permite capturar cualquier argumento que que django le pase al metodo
-        # Calcula el valor total antes de guardar
+    def save(self, *args, **kwargs): 
+        # Calcular valor total
         self.valorTotal = self.precioUnitario * self.cantidad
+
+        # Descontar del stock si es un nuevo registro
+        if self.fk_Cosecha and self.fk_UnidadMedida and self._state.adding:
+            cantidad_en_base = self.cantidad * self.fk_UnidadMedida.equivalenciabase
+            self.fk_Cosecha.cantidadDisponible -= cantidad_en_base
+            self.fk_Cosecha.save()
+
         super().save(*args, **kwargs)
 
     def __str__(self):
