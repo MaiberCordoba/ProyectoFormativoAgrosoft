2fb16dd6e8fa4d9f67de39e9133de929a3e5bbb0
maiberCordoba <cordobamaiber@gmail.com>
2025-04-10 23:38:13 -0500

    arreglando error en tipoActividad del mx de caracteres

--- backend/apps/finanzas/api/models/tipoActividad.py
+++ backend/apps/finanzas/api/models/tipoActividad.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class TipoActividad(models.Model):
-    nombre = models.CharField(70)
+    nombre = models.CharField(max_length=70)
 
     def __str__(self):
         return self.nombre
