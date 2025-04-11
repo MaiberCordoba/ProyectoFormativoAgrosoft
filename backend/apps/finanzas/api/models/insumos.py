32ed171ce12fedd87ab50f1787ba426ccc0c44cc
maiberCordoba <cordobamaiber@gmail.com>
2025-04-10 20:11:00 -0500

    insumos finalizado con los calculos correspondientes para cantidadTotal de insumos y el valor total

--- backend/apps/finanzas/api/models/insumos.py
+++ backend/apps/finanzas/api/models/insumos.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
+from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
 
 
 class Insumos(models.Model):
@@ -7,9 +7,19 @@ class Insumos(models.Model):
     descripcion = models.TextField(max_length=150)
     precio = models.IntegerField()
     compuestoActivo = models.CharField(max_length=20)
-    contenido = models.IntegerField()
+    contenido = models.FloatField()
     fichaTecnica = models.CharField(max_length=20)
     unidades = models.IntegerField()
-#    fk_UnidadMedida = models.ForeignKey(UnidadesMedida , on_delete=models.SET_NULL, null=True)
+    fk_UnidadMedida = models.ForeignKey(UnidadesMedida , on_delete=models.SET_NULL, null=True)
+    valorTotalInsumos = models.FloatField()
+    cantidadTotal = models.FloatField()
+    
+    def save(self, *args, **kwargs):
+        
+        self.cantidadTotal = self.contenido * self.fk_UnidadMedida.equivalenciabase * self.unidades
+        self.valorTotalInsumos = self.precio * self.unidades
+
+        super().save(*args, **kwargs)
+        
     def __str__(self):
         return self.nombre
