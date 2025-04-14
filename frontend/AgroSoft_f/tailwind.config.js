644de1cc1179e8f05a7d70ff4380fe0cbd85ed1f
maiberCordoba <cordobamaiber@gmail.com>
2025-04-14 15:19:48 -0500

    agregando verde institucional para manejar dentro del navbar

--- frontend/AgroSoft_f/tailwind.config.js
+++ frontend/AgroSoft_f/tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sena-green": "#2CC302",
+      },
+    },
   },
   darkMode: "class",
   plugins: [
