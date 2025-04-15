c866e4ad10ad1b2b94ccfa9abd3b8fc31106db1b
maiberCordoba <cordobamaiber@gmail.com>
2025-04-14 21:25:01 -0500

    agregando validacion al editar tipo plaga borre la anterior img almacena en el registro, para no duplicar varias img en un solo registro

--- backend/apps/sanidad/api/views/tipoPlagaViews.py
+++ backend/apps/sanidad/api/views/tipoPlagaViews.py
@@ -8,3 +8,11 @@ class TipoPlagaModelViewSet(ModelViewSet):
     serializer_class = TipoPlagaModelSerializer
     queryset = tipoPlaga.objects.all()
     
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        # Si llega una nueva imagen, elimina la anterior
+        if 'img' in request.FILES and instance.img:
+            instance.img.delete(save=False)
+
+        return super().update(request, *args, **kwargs)
