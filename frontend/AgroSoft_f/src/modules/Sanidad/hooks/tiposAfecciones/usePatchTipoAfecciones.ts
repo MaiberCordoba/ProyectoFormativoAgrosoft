2275510c80ae94f47f72d26552911af2f4e73961
maiberCordoba <cordobamaiber@gmail.com>
2025-04-14 21:10:45 -0500

    manejando form data en los hooks de enviar y editar tipos afecciones

--- frontend/AgroSoft_f/src/modules/Sanidad/hooks/tiposAfecciones/usePatchTipoAfecciones.ts
+++ frontend/AgroSoft_f/src/modules/Sanidad/hooks/tiposAfecciones/usePatchTipoAfecciones.ts
@@ -6,7 +6,7 @@ import { addToast } from "@heroui/react";
 export const usePatchTipoAfecciones = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<TiposAfecciones, Error, { id: number; data: Partial<TiposAfecciones> }>({
+  return useMutation<TiposAfecciones, Error, { id: number; data: FormData }>({
     mutationFn: ({ id, data }) => patchTipoAfecciones(id, data),
     onSuccess: (updatedTipoAfeccion, variables) => {
       // Actualiza la caché después de una mutación exitosa
@@ -21,7 +21,7 @@ export const usePatchTipoAfecciones = () => {
       addToast({
         title: "Actualización exitosa",
         description: "el tipo de afeccion se actualizó correctamente",
-        color: "success",
+        color: "primary",
      
       });
     },
