cc3be6019948880d28bd9811f64470b907a80608
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-04-17 12:18:55 -0500

    Creando models o para la tabla movimientoInventario

---
+++ backend/apps/finanzas/api/models/movimientosInventario.py
@@ -0,0 +1,22 @@
+from django.db import models
+from apps.finanzas.api.models.cosechas import Cosechas
+from apps.trazabilidad.api.models.HerramientasModel import Herramientas
+from apps.users.models import Usuario
+from apps.finanzas.api.models.insumos import Insumos
+
+class MovimientoInventario(models.Model):
+
+    TIPO_CHOICES = [
+        ('entrada','Entrada'),
+        ('salida','Salida')
+    ]
+    tipo = models.CharField(max_length=30, choices=TIPO_CHOICES)
+    cantidad = models.IntegerField()
+    descripcion = models.TextField(max_length=300)
+    fk_Insumo = models.ForeignKey(Insumos,models.SET_NULL,null=True)
+    fk_Herramienta = models.ForeignKey(Herramientas,models.SET_NULL,null=True)
+    fk_Cosecha = models.ForeignKey(Cosechas,models.SET_NULL,null=True)
+    fk_Usuario = models.ForeignKey(Usuario,models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.descripcion
