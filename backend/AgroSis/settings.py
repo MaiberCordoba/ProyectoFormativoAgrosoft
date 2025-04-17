cdebed75efda2f3fa45ac6031ff3be283f523e03
maiberCordoba <cordobamaiber@gmail.com>
2025-04-16 22:42:36 -0500

    agregando ruta de front end para manejo de enviar correos de  recuperar contraseña

--- backend/AgroSis/settings.py
+++ backend/AgroSis/settings.py
@@ -175,6 +175,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+FRONTEND_URL = "http://localhost:5173"
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
