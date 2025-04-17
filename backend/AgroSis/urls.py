f77f5eea572be4a44cfec0ac861535497c27ad40
Juniorr-7 <juniorfajardom@gmail.com>
2025-04-16 20:15:48 -0500

    urls para dicha recuperacion de contraseña

--- backend/AgroSis/urls.py
+++ backend/AgroSis/urls.py
@@ -81,6 +81,7 @@ from apps.finanzas.api.routers.routerUnidadesMedida import routerUnidadesMedida
 
 #Usuarios
 from apps.users.urls import router_usuarios
+from apps.users.api.forgotPassword import solicitar_recuperacion, resetear_contraseña
 
 
 urlpatterns = [
@@ -131,10 +132,13 @@ urlpatterns = [
     
      #Usuarios
     path('api/',include(router_usuarios.urls)),
+    path("api/solicitar-recuperacion/", solicitar_recuperacion, name="solicitar_recuperacion"),
+    path("api/resetear-contrasena/", resetear_contraseña, name="resetear_contraseña"),
 
     # Ruta para obtener el token JWT
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     
     # Ruta para refrescar el token JWT
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'), 
+
 ]
