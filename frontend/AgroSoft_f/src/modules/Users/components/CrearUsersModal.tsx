419c8bef5d017f9e9b4c000cf5f716fb58d7c4b5
maiberCordoba <cordobamaiber@gmail.com>
2025-04-18 18:37:58 -0500

    argregando el campo estado en el registro de usuarios

--- frontend/AgroSoft_f/src/modules/Users/components/CrearUsersModal.tsx
+++ frontend/AgroSoft_f/src/modules/Users/components/CrearUsersModal.tsx
@@ -1,6 +1,6 @@
 import { useState, ChangeEvent } from "react";
 import ModalComponent from "@/components/Modal";
-import { Input, Checkbox } from "@heroui/react";
+import { Input, Checkbox, Select, SelectItem } from "@heroui/react";
 import { usePostUsers } from "../hooks/usePostUsers";
 
 interface CrearUsersModalProps {
@@ -8,6 +8,7 @@ interface CrearUsersModalProps {
 }
 
 interface UserFormState {
+  id:number,
   identificacion: string;
   nombre: string;
   apellidos: string;
@@ -16,10 +17,12 @@ interface UserFormState {
   correoElectronico: string;
   password: string;
   admin: boolean;
+  estado: string;
 }
 
 export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
   const [userData, setUserData] = useState<UserFormState>({
+    id:0,
     identificacion: "",
     nombre: "",
     apellidos: "",
@@ -27,6 +30,7 @@ export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
     telefono: "",
     correoElectronico: "",
     password: "",
+    estado: "",
     admin: false
   });
 
@@ -59,6 +63,7 @@ export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
       onSuccess: () => {
         onClose();
         setUserData({
+          id:0,
           identificacion: "",
           nombre: "",
           apellidos: "",
@@ -66,6 +71,7 @@ export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
           telefono: "",
           correoElectronico: "",
           password: "",
+          estado:"",
           admin: false
         });
       }
@@ -139,6 +145,22 @@ export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
           onChange={(e) => handleInputChange(e, 'password')}
           required
         />
+
+        <Select
+          isRequired
+          label="Estado"
+          selectedKeys={new Set([userData.estado])} // Muestra el valor actual
+          onSelectionChange={(keys) => {
+            const selectedValue = Array.from(keys)[0] as string;
+            setUserData(prev => ({
+              ...prev,
+              estado: selectedValue
+            }));
+          }}
+        >
+          <SelectItem key="activo" >Activo</SelectItem>
+          <SelectItem key="inactivo" >Inactivo</SelectItem>
+        </Select>
 
         <div className="flex items-center mt-2">
           <Checkbox
