9f37f0a76fb6432f356b183fd0b9530ab7c810d5
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-04-18 23:27:13 -0500

    Pasando logica del modelo al serializer para mantener el modelo mas limpio

--- backend/apps/finanzas/api/models/movimientosInventario.py
+++ backend/apps/finanzas/api/models/movimientosInventario.py
@@ -5,18 +5,18 @@ from apps.users.models import Usuario
 from apps.finanzas.api.models.insumos import Insumos
 
 class MovimientoInventario(models.Model):
-
     TIPO_CHOICES = [
-        ('entrada','Entrada'),
-        ('salida','Salida')
+        ('entrada', 'Entrada'),
+        ('salida', 'Salida')
     ]
     tipo = models.CharField(max_length=30, choices=TIPO_CHOICES)
     cantidad = models.IntegerField()
     descripcion = models.TextField(max_length=300)
-    fk_Insumo = models.ForeignKey(Insumos,models.SET_NULL,null=True)
-    fk_Herramienta = models.ForeignKey(Herramientas,models.SET_NULL,null=True)
-    fk_Cosecha = models.ForeignKey(Cosechas,models.SET_NULL,null=True)
-    fk_Usuario = models.ForeignKey(Usuario,models.SET_NULL,null=True)
+    fk_Insumo = models.ForeignKey(Insumos, models.SET_NULL, null=True, blank=True)
+    fk_Herramienta = models.ForeignKey(Herramientas, models.SET_NULL, null=True, blank=True)
+    fk_Cosecha = models.ForeignKey(Cosechas, models.SET_NULL, null=True, blank=True)
+    fk_Usuario = models.ForeignKey(Usuario, models.SET_NULL, null=True, blank=True)
 
+    
     def __str__(self):
         return self.descripcion
--- backend/apps/finanzas/api/models/usosInsumos.py
+++ backend/apps/finanzas/api/models/usosInsumos.py
@@ -7,31 +7,17 @@ from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
 
 class UsosInsumos(models.Model):
     fk_Insumo = models.ForeignKey(Insumos, on_delete=models.SET_NULL, null=True)
-    fk_Actividad = models.ForeignKey(Actividades, on_delete=models.SET_NULL, null=True,blank=True)
-    fk_Control = models.ForeignKey(Controles, on_delete=models.SET_NULL, null=True,blank=True)
+    fk_Actividad = models.ForeignKey(Actividades, on_delete=models.SET_NULL, null=True, blank=True)
+    fk_Control = models.ForeignKey(Controles, on_delete=models.SET_NULL, null=True, blank=True)
     cantidadProducto = models.IntegerField()
-    fk_UnidadMedida = models.ForeignKey(UnidadesMedida, on_delete=models.SET_NULL,null=True)
-    costoUsoInsumo = models.FloatField(null=True,blank=True)
-   
-    
+    fk_UnidadMedida = models.ForeignKey(UnidadesMedida, on_delete=models.SET_NULL, null=True)
+    costoUsoInsumo = models.FloatField(null=True, blank=True)
+
     def clean(self):
         if self.fk_Actividad and self.fk_Control:
             raise ValidationError("Solo se puede relacionar con una actividad o un control, no ambos.")
         if not self.fk_Actividad and not self.fk_Control:
             raise ValidationError("Debe relacionarse con una actividad o un control.")
-        
-    def save(self, *args, **kwargs):
-        if self.fk_Insumo and self.fk_UnidadMedida:
-        # Conversión a unidad base
-            cantidad_en_base = self.cantidadProducto * self.fk_UnidadMedida.equivalenciabase
-            self.costoUsoInsumo = (cantidad_en_base*self.fk_Insumo.valorTotalInsumos)/self.fk_Insumo.cantidadTotal
 
-             # Resta del stock
-            if self._state.adding: 
-                self.fk_Insumo.cantidadDisponible -= cantidad_en_base
-                self.fk_Insumo.save()
-
-        super().save(*args, **kwargs)
-    
     def __str__(self):
-        return f"insumo: {self.fk_Insumo}-Cantidad: {self.cantidadProducto}"
+        return f"Insumo: {self.fk_Insumo} - Cantidad: {self.cantidadProducto}"
