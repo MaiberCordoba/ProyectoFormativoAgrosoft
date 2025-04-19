93b6ab5accb2df82e2baa655706ec633d8598ec2
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-04-18 23:28:43 -0500

    Agregando logica dentro del serializer para que trabaje el backend y no la base de datos

--- backend/apps/finanzas/api/serializers/serializerSalarios.py
+++ backend/apps/finanzas/api/serializers/serializerSalarios.py
@@ -1,7 +1,23 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from apps.finanzas.api.models.salarios import Salarios
 
 class SerializerSalarios(ModelSerializer):
+
+    monto_minutos = serializers.FloatField(read_only=True)  # ✅ Aquí el cambio
+
     class Meta:
         model = Salarios
-        field = '__all__'
+        fields = '__all__'
+
+    def create(self, validated_data):
+        horas = validated_data.get('horas')
+        monto = validated_data.get('monto')
+        validated_data['monto_minutos'] = monto / (horas * 60)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        horas = validated_data.get('horas', instance.horas)
+        monto = validated_data.get('monto', instance.monto)
+        validated_data['monto_minutos'] = monto / (horas * 60)
+        return super().update(instance, validated_data)
