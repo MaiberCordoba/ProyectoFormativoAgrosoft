2f0fa8ae9464c049579e52e28f71d71b54b1cdc9
maiberCordoba <cordobamaiber@gmail.com>
2025-04-20 18:08:04 -0500

    montando consulta para mostrar un resumen de todos los cultivos en beneficio costo

--- backend/AgroSis/urls.py
+++ backend/AgroSis/urls.py
@@ -84,6 +84,7 @@ from apps.finanzas.api.routers.routerUnidadesTiempo import routerUnidadesTiempo
 from apps.finanzas.api.routers.routerUnidadesMedida import routerUnidadesMedida
 from apps.finanzas.api.routers.routerMovimientoInventario import routerMovimientoInventario
 from apps.finanzas.api.routers.routerBeneficioCosto import routerBeneficioCosto
+from apps.finanzas.api.routers.routerListBeneficioCosto import routerListBeneficioCosto
 
 #Usuarios
 from apps.users.urls import router_usuarios
@@ -137,6 +138,7 @@ urlpatterns = [
     path('api/',include(routerUnidadesMedida.urls)),
     path('api/',include(routerMovimientoInventario.urls)),
     path('api/',include(routerBeneficioCosto.urls)),
+    path('api/',include(routerListBeneficioCosto.urls)),
     
      #Usuarios
     path('api/',include(router_usuarios.urls)),
