51e326711591eecd7edca782338b1fba82580023
sabinicolas05 <nsabirojas@gmail.com>
2025-04-24 14:16:53 -0500

    solucione algunos errores que tenia el backen en cosechas y insumos

--- backend/apps/finanzas/api/models/cosechas.py
+++ backend/apps/finanzas/api/models/cosechas.py
@@ -5,7 +5,7 @@ from apps.finanzas.api.models.unidadesMedida import UnidadesMedida
 class Cosechas(models.Model):
     fk_Cultivo = models.ForeignKey(Cultivos, on_delete=models.SET_NULL, null=True)
     fk_UnidadMedida = models.ForeignKey(UnidadesMedida, on_delete=models.SET_NULL, null=True)
-    cantidad = models.IntegerField()
+    cantidad = models.IntegerField(null=True)
     cantidadTotal = models.FloatField(null=True, blank=True)
     cantidadDisponible = models.FloatField(default=0)
     fecha = models.DateField(auto_now=False)
