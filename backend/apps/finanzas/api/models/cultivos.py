82a05411d9d7eb10145cd36d447ba04522210b15
Mamuel17 <rubyesperanza596@gmail.com>
2025-05-01 18:21:59 -0500

    quitando el nombre del modelo de cutivo

--- backend/apps/finanzas/api/models/cultivos.py
+++ backend/apps/finanzas/api/models/cultivos.py
@@ -5,7 +5,6 @@ from apps.trazabilidad.api.models.SemillerosModel import Semilleros
 class Cultivos(models.Model):
     fk_Especie = models.ForeignKey(Especies,on_delete=models.SET_NULL, null=True)
     fk_semillero = models.ForeignKey(Semilleros,on_delete=models.SET_NULL, null=True)
-    nombre = models.CharField(max_length=30)
     unidades = models.IntegerField()
     activo = models.BooleanField(default=False, null=False)
     fechaSiembra = models.DateField(auto_now=False)
