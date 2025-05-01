4a7fc9514bc317d9914be636da7124dd605e52d9
karensemanate <karen.semanate@soy.sena.edu.co>
2025-05-01 17:07:23 -0500

    arregle nuevamente el modelo de umbrales

--- backend/apps/electronica/api/models/umbral.py
+++ backend/apps/electronica/api/models/umbral.py
@@ -1,13 +1,17 @@
 from django.db import models
 from apps.electronica.api.models.sensor import Sensor
+from django.core.exceptions import ValidationError
 
-
-class Umbral (models.Model):
-    sensor =  models.ForeignKey(Sensor, on_delete=models.SET_NULL, null=True) 
+class Umbral(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.SET_NULL, null=True)
     valor_minimo = models.DecimalField(max_digits=10, decimal_places=2)
     valor_maximo = models.DecimalField(max_digits=10, decimal_places=2)
 
-
+    def clean(self):
+        if self.valor_minimo >= self.valor_maximo:
+            raise ValidationError("El valor mínimo debe ser menor al valor máximo.")
+    
     def __str__(self):
-        return f'{self.sensor_id} - Valor mínimo: {self.valor_minimo}, Valor máximo: {self.valor_maximo}'
+        return f'{self.sensor.id} - Valor mínimo: {self.valor_minimo}, Valor máximo: {self.valor_maximo}'
+
 
