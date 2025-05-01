21d18a5254d7ed7b3db3d25539e20328f1e06f39
Mamuel17 <rubyesperanza596@gmail.com>
2025-05-01 18:22:56 -0500

    agregue una funcion def en en views para que se pueda cambia la imagen en la vista

--- backend/apps/trazabilidad/api/models/EspeciesModel.py
+++ backend/apps/trazabilidad/api/models/EspeciesModel.py
@@ -2,10 +2,20 @@ from django.db import models
 from .TiposEspecieModel import TiposEspecie
 
 class Especies(models.Model):
-    fk_tipoespecie = models.ForeignKey(TiposEspecie,on_delete=models.SET_NULL,null=True)
+    class TiempoCrecimientoChoices(models.TextChoices):
+        PERENNES = 'perennes', 'Perennes'
+        SEMIPERENNES= 'semiperennes', 'Semiperennes'
+        TRANSITORIO = 'transitorio', 'Transitorio'
+
+    fk_tipoespecie = models.ForeignKey(TiposEspecie, on_delete=models.SET_NULL, null=True)
     nombre = models.CharField(max_length=30)
     descripcion = models.TextField(max_length=400)
-    models.ImageField(upload_to="especies/", null=True, blank=True)
-    tiempocrecimiento = models.IntegerField()
+    img = models.ImageField(upload_to="especies/", null=True, blank=True)
+    tiempocrecimiento = models.CharField(
+        max_length=15,
+        choices=TiempoCrecimientoChoices.choices,
+        default=TiempoCrecimientoChoices.PERENNES
+    )
+
     def __str__(self):
         return self.nombre
