da3cbf16b4a7bae4b71f1013788f730e2fcfa20e
Mamuel17 <rubyesperanza596@gmail.com>
2025-05-01 13:07:16 -0500

    quitando el campo de variedad a especies para crear el modelo de variedad

--- backend/apps/trazabilidad/api/models/EspeciesModel.py
+++ backend/apps/trazabilidad/api/models/EspeciesModel.py
@@ -5,7 +5,6 @@ class Especies(models.Model):
     fk_tipoespecie = models.ForeignKey(TiposEspecie,on_delete=models.SET_NULL,null=True)
     nombre = models.CharField(max_length=30)
     descripcion = models.TextField(max_length=400)
-    variedad = models.TextField(max_length=400)
     img = models.CharField(max_length=255)
     tiempocrecimiento = models.IntegerField()
     def __str__(self):
