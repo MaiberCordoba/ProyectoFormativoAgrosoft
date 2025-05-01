446e4bc4da01f663b615dcf5920c3a919578e0d1
karensemanate <karen.semanate@soy.sena.edu.co>
2025-05-01 17:06:27 -0500

    arregle el modelo y las views de los umbrales

--- backend/apps/electronica/api/serializers/umbral_serializer.py
+++ backend/apps/electronica/api/serializers/umbral_serializer.py
@@ -4,12 +4,17 @@ from apps.electronica.api.serializers.Sensor_Serializer import *
 from rest_framework import serializers
 
 
-class UmbralSerializer(ModelSerializer):
+class UmbralSerializer(serializers.ModelSerializer):
     sensor = SensorSerializer(read_only=True)
     sensor_id = serializers.PrimaryKeyRelatedField(
         queryset=Sensor.objects.all(), source='sensor', write_only=True
     )
+
     class Meta:
         model = Umbral
         fields = ['id', 'sensor', 'sensor_id', 'valor_minimo', 'valor_maximo']
 
+    def validate(self, data):
+        if data.get('valor_minimo') >= data.get('valor_maximo'):
+            raise serializers.ValidationError("El valor mínimo debe ser menor al valor máximo.")
+        return data
--- backend/apps/electronica/api/views/umbral_views.py
+++ backend/apps/electronica/api/views/umbral_views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.electronica.api.models.umbral import *
 from apps.electronica.api.serializers.umbral_serializer import *
-from rest_framework.permissions import IsAuthenticated
+#from rest_framework.permissions import IsAuthenticated
 
 class Erasview(ModelViewSet):
     queryset = Umbral.objects.all()
