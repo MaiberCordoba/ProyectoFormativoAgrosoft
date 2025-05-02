30dc2f58c6d3396e30362ab0d6de4c60aae0cd23
sabinicolas05 <nsabirojas@gmail.com>
2025-05-02 11:11:52 -0500

    arregle el serialix¿zer de cultivo para que me traiga la infromacion que tienen sus rrespectvas llaves foranea

--- backend/apps/finanzas/api/serializers/serializerCultivos.py
+++ backend/apps/finanzas/api/serializers/serializerCultivos.py
@@ -1,7 +1,8 @@
 from rest_framework.serializers import ModelSerializer
 from apps.finanzas.api.models.cultivos import Cultivos
-
+from apps.trazabilidad.api.serializers.SemillerosSerializer import SemillerosSerializer
 class SerializerCultivos(ModelSerializer):
+    semillero = SemillerosSerializer(source='fk_Semillero',read_only=True)
     class Meta:
         model = Cultivos
         fields = '__all__'
