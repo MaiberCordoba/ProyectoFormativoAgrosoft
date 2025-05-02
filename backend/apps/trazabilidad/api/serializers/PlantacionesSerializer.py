d4526da1e391eef450493e6eb48c0a11b602f5a4
sabinicolas05 <nsabirojas@gmail.com>
2025-05-02 11:13:44 -0500

    arregle el serializer de plantaciones paravque me traiga  toda la info de sus respectivas llaves foraneas

--- backend/apps/trazabilidad/api/serializers/PlantacionesSerializer.py
+++ backend/apps/trazabilidad/api/serializers/PlantacionesSerializer.py
@@ -1,7 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 from ..models.PlantacionesModel import Plantaciones
+from  apps.finanzas.api.serializers.serializerCultivos import SerializerCultivos
+from apps.electronica.api.serializers.Eras_Seralizer import ErasSerializer
 
 class PlantacionesSerializer(ModelSerializer):
+    cultivo = SerializerCultivos(source='fk_Cultivo',read_only=True)
+    eras = ErasSerializer(source='fk_Era',read_only=True)
     class Meta:
         model = Plantaciones
         fields = '__all__'
