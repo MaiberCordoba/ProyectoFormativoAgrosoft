4a76ef38c7b604669dc2e84b6171b8491cf5a1cf
sabinicolas05 <nsabirojas@gmail.com>
2025-05-02 11:10:39 -0500

    arregle el serializer de semillero para que me trajera todos los datos de la llave foraneo de especie

--- backend/apps/trazabilidad/api/serializers/SemillerosSerializer.py
+++ backend/apps/trazabilidad/api/serializers/SemillerosSerializer.py
@@ -1,7 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 from ..models.SemillerosModel import Semilleros
+from apps.trazabilidad.api.serializers.EspeciesSerializer import EspeciesSerializer
 
 class SemillerosSerializer(ModelSerializer):
+    especie = EspeciesSerializer(source='fk_especie',read_only=True)
     class Meta:
         model = Semilleros
         fields = '__all__'
