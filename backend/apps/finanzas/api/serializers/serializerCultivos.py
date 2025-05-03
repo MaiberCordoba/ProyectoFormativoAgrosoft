ffd65b8bbe6292036c3cf379eb2580ca6188f98c
maiberCordoba <cordobamaiber@gmail.com>
2025-05-02 19:54:01 -0500

    organizando informacion para mostar aparte de las fk anidar la info de las tablas anidadas

--- backend/apps/finanzas/api/serializers/serializerCultivos.py
+++ backend/apps/finanzas/api/serializers/serializerCultivos.py
@@ -1,8 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 from apps.finanzas.api.models.cultivos import Cultivos
-from apps.trazabilidad.api.serializers.SemillerosSerializer import SemillerosSerializer
+from apps.trazabilidad.api.serializers.EspeciesSerializer import EspeciesSerializer
+
 class SerializerCultivos(ModelSerializer):
-    semillero = SemillerosSerializer(source='fk_Semillero',read_only=True)
+    especies = EspeciesSerializer(source='fk_Especie',read_only=True)
     class Meta:
         model = Cultivos
         fields = '__all__'
