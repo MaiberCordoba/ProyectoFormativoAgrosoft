5e181f485ea032d1ccb5f9a839438bf895205b46
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-05-08 18:12:42 -0500

    Creando page para movimientos inventario

---
+++ frontend/AgroSoft_f/src/modules/Finanzas/pages/pageMovimientoInventario.tsx
@@ -0,0 +1,9 @@
+import { MovimientosList } from "../components/movimientosInventario/movimientosInventarioList";
+
+export function MovimientosInventario(){
+    return(
+        <div>
+            <MovimientosList/>
+        </div>
+    )
+}
