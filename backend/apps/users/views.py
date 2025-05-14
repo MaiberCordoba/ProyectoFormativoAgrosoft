8cd448c3adce965ffe7cc30c717748fd749c8c93
Juniorr-7 <juniorfajardom@gmail.com>
2025-05-14 11:39:15 -0500

    agregando el registro de la carga masiva de usuarios

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,9 +1,14 @@
 from rest_framework.decorators import action
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Usuario
 from .serializers import UsuarioSerializer
+import pandas as pd
+from rest_framework.parsers import MultiPartParser
+from rest_framework.views import APIView 
+
 
 class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
@@ -19,3 +24,54 @@ class UsuarioViewSet(ModelViewSet):
         """Devuelve la información del usuario autenticado"""
         serializer = UsuarioSerializer(request.user)
         return Response(serializer.data)
+
+class RegistroMasivoUsuariosView(APIView):
+    parser_classes = [MultiPartParser]
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        archivo = request.FILES.get('archivo')
+        if not archivo:
+            return Response({'error': 'No se proporcionó ningún archivo.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            df = pd.read_excel(archivo)
+        except Exception as e:
+            return Response({'error': f'Error al leer el archivo: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if df.empty:
+            return Response({'error': 'El archivo no contiene datos.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        errores = []
+        for index, row in df.iterrows():
+            fila = index + 2  
+
+            campos_requeridos = ['nombre', 'apellido', 'email', 'username', 'password']
+            faltantes = [campo for campo in campos_requeridos if not row.get(campo)]
+
+            if faltantes:
+                errores.append({
+                    'fila': fila,
+                    'errores': f"Campos vacíos: {', '.join(faltantes)}"
+                })
+                continue  # saltamos al siguiente
+
+            data = {
+                'nombre': row.get('nombre'),
+                'apellido': row.get('apellido'),
+                'email': row.get('email'),
+                'username': row.get('username'),
+                'password': row.get('password'),
+            }
+
+            serializer = UsuarioSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                errores.append({'fila': fila, 'errores': serializer.errors})
+
+        if errores:
+            return Response({'mensaje': 'Algunos usuarios no se pudieron registrar.', 'errores': errores}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'mensaje': 'Todos los usuarios se registraron exitosamente.'}, status=status.HTTP_201_CREATED)
+       
