c993e7d14f4686c8339ff72521ee8d54011438b2
maiberCordoba <cordobamaiber@gmail.com>
2025-05-24 15:59:12 -0500

    botones de modales solidos

--- frontend/AgroSoft_f/src/modules/Users/components/CrearUsersModal.tsx
+++ frontend/AgroSoft_f/src/modules/Users/components/CrearUsersModal.tsx
@@ -8,7 +8,7 @@ interface CrearUsersModalProps {
 }
 
 interface UserFormState {
-  id:number,
+  id: number;
   identificacion: string;
   nombre: string;
   apellidos: string;
@@ -22,7 +22,7 @@ interface UserFormState {
 
 export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
   const [userData, setUserData] = useState<UserFormState>({
-    id:0,
+    id: 0,
     identificacion: "",
     nombre: "",
     apellidos: "",
@@ -31,31 +31,38 @@ export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
     correoElectronico: "",
     password: "",
     estado: "",
-    admin: false
+    admin: false,
   });
 
   const { mutate, isPending } = usePostUsers();
-  const today = new Date().toISOString().split('T')[0];
+  const today = new Date().toISOString().split("T")[0];
 
   // Maneja cambios en inputs normales
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>, field: keyof Omit<UserFormState, 'admin'>) => {
-    setUserData(prev => ({
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    field: keyof Omit<UserFormState, "admin">
+  ) => {
+    setUserData((prev) => ({
       ...prev,
-      [field]: e.target.value
+      [field]: e.target.value,
     }));
   };
 
   // Maneja cambio específico del checkbox
   const handleAdminChange = (isChecked: boolean) => {
-    setUserData(prev => ({
+    setUserData((prev) => ({
       ...prev,
-      admin: isChecked
+      admin: isChecked,
     }));
   };
 
   const handleSubmit = () => {
-    if (!userData.identificacion || !userData.nombre || 
-        !userData.correoElectronico || !userData.password) {
+    if (
+      !userData.identificacion ||
+      !userData.nombre ||
+      !userData.correoElectronico ||
+      !userData.password
+    ) {
       console.log("Por favor, completa los campos requeridos.");
       return;
     }
@@ -64,7 +71,7 @@ export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
       onSuccess: (data) => {
         onClose();
         setUserData({
-          id:0,
+          id: 0,
           identificacion: "",
           nombre: "",
           apellidos: "",
@@ -72,13 +79,12 @@ export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
           telefono: "",
           correoElectronico: "",
           password: "",
-          estado:"",
-          admin: false
+          estado: "",
+          admin: false,
         });
-      }
+      },
     });
   };
-  
 
   return (
     <ModalComponent
@@ -89,91 +95,86 @@ export const CrearUsersModal = ({ onClose }: CrearUsersModalProps) => {
         {
           label: isPending ? "Guardando..." : "Guardar",
           color: "success",
-          variant: "light",
-          onClick: handleSubmit
+          variant: "solid",
+          onClick: handleSubmit,
         },
       ]}
     >
-      
-        <Input
-          label="Identificación"
-          type="number"
-          value={userData.identificacion}
-          onChange={(e) => handleInputChange(e, 'identificacion')}
-          required
-        />
+      <Input
+        label="Identificación"
+        type="number"
+        value={userData.identificacion}
+        onChange={(e) => handleInputChange(e, "identificacion")}
+        required
+      />
 
-        <Input
-          label="Nombre"
-          type="text"
-          value={userData.nombre}
-          onChange={(e) => handleInputChange(e, 'nombre')}
-          required
-        />
+      <Input
+        label="Nombre"
+        type="text"
+        value={userData.nombre}
+        onChange={(e) => handleInputChange(e, "nombre")}
+        required
+      />
 
-        <Input
-          label="Apellidos"
-          type="text"
-          value={userData.apellidos}
-          onChange={(e) => handleInputChange(e, 'apellidos')}
-        />
+      <Input
+        label="Apellidos"
+        type="text"
+        value={userData.apellidos}
+        onChange={(e) => handleInputChange(e, "apellidos")}
+      />
 
-        <Input
-          label="Fecha de Nacimiento"
-          type="date"
-          value={userData.fechaNacimiento}
-          onChange={(e) => handleInputChange(e, 'fechaNacimiento')}
-           max={today}
-        />
+      <Input
+        label="Fecha de Nacimiento"
+        type="date"
+        value={userData.fechaNacimiento}
+        onChange={(e) => handleInputChange(e, "fechaNacimiento")}
+        max={today}
+      />
 
-        <Input
-          label="Teléfono"
-          type="tel"
-          value={userData.telefono}
-          onChange={(e) => handleInputChange(e, 'telefono')}
-        />
+      <Input
+        label="Teléfono"
+        type="tel"
+        value={userData.telefono}
+        onChange={(e) => handleInputChange(e, "telefono")}
+      />
 
-        <Input
-          label="Correo Electrónico"
-          type="email"
-          value={userData.correoElectronico}
-          onChange={(e) => handleInputChange(e, 'correoElectronico')}
-          required
-        />
+      <Input
+        label="Correo Electrónico"
+        type="email"
+        value={userData.correoElectronico}
+        onChange={(e) => handleInputChange(e, "correoElectronico")}
+        required
+      />
 
-        <Input
-          label="Contraseña"
-          type="password"
-          value={userData.password}
-          onChange={(e) => handleInputChange(e, 'password')}
-          required
-        />
+      <Input
+        label="Contraseña"
+        type="password"
+        value={userData.password}
+        onChange={(e) => handleInputChange(e, "password")}
+        required
+      />
 
-        <Select
-          isRequired
-          label="Estado"
-          selectedKeys={new Set([userData.estado])} // Muestra el valor actual
-          onSelectionChange={(keys) => {
-            const selectedValue = Array.from(keys)[0] as string;
-            setUserData(prev => ({
-              ...prev,
-              estado: selectedValue
-            }));
-          }}
-        >
-          <SelectItem key="activo" >Activo</SelectItem>
-          <SelectItem key="inactivo" >Inactivo</SelectItem>
-        </Select>
+      <Select
+        isRequired
+        label="Estado"
+        selectedKeys={new Set([userData.estado])} // Muestra el valor actual
+        onSelectionChange={(keys) => {
+          const selectedValue = Array.from(keys)[0] as string;
+          setUserData((prev) => ({
+            ...prev,
+            estado: selectedValue,
+          }));
+        }}
+      >
+        <SelectItem key="activo">Activo</SelectItem>
+        <SelectItem key="inactivo">Inactivo</SelectItem>
+      </Select>
 
-        <div className="flex items-center mt-2">
-          <Checkbox
-            isSelected={userData.admin}
-            onValueChange={handleAdminChange}
-          >
-            ¿Es administrador?
-          </Checkbox>
-        </div>
-   
+      <div className="flex items-center mt-2">
+        <Checkbox isSelected={userData.admin} onValueChange={handleAdminChange}>
+          ¿Es administrador?
+        </Checkbox>
+      </div>
     </ModalComponent>
   );
 };
