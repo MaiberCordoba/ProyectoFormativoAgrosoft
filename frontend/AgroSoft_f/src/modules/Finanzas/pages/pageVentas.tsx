8d6e3659e3895b99dd2ff83a2e7eef98f5c1650e
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-05-28 10:54:29 -0500

    Agregando tarjetas card para mostrar informacion

--- frontend/AgroSoft_f/src/modules/Finanzas/pages/pageVentas.tsx
+++ frontend/AgroSoft_f/src/modules/Finanzas/pages/pageVentas.tsx
@@ -1,20 +1,12 @@
 import { CosechasResumenCard } from "../CardFinanzas";
 import { VentasList } from "../components/ventas/VentasList";
-import { useQuery } from "@tanstack/react-query";
 
 export function Ventas() {
-  const { data: cosechas = [], isLoading } = useQuery({
-    queryKey: ['cosechas'],
-    queryFn: () => fetch("http://127.0.0.1:8000/api/cosechas").then((res) => res.json())
-  });
-
   return (
-    <div className="p-4">
-      {isLoading ? (
-        <p>Cargando cosechas...</p>
-      ) : (
-        <CosechasResumenCard cosechas={cosechas} />
-      )}
+    <div>
+      <div className="p-4">
+        <CosechasResumenCard/>
+      </div>
       <VentasList />
     </div>
   );
