c26d9d6f59fb415e34428aaa0345a9a374386e69
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-05-28 10:56:50 -0500

    Agregando plantaciones al editar actividad

--- frontend/AgroSoft_f/src/modules/Finanzas/components/actividades/EditarActividadesModal.tsx
+++ frontend/AgroSoft_f/src/modules/Finanzas/components/actividades/EditarActividadesModal.tsx
@@ -6,6 +6,7 @@ import { Input, Textarea, Select, SelectItem } from '@heroui/react';
 import { useGetCultivos } from '@/modules/Trazabilidad/hooks/cultivos/useGetCultivos'; 
 import { useGetUsers } from '@/modules/Users/hooks/useGetUsers';
 import { useGetTipoActividad } from '../../hooks/tipoActividad/useGetTiposActividad';
+import { useGetPlantaciones } from '@/modules/Trazabilidad/hooks/plantaciones/useGetPlantaciones';
 
 interface EditarActividadesModalProps {
   actividad: Actividades;
@@ -17,22 +18,29 @@ const EditarActividadesModal: React.FC<EditarActividadesModalProps> = ({ activid
   const [descripcion, setDescripcion] = useState<string>(actividad.descripcion);
   const [estado, setEstado] = useState<"AS" | "CO" | "CA">(actividad.estado);
   const [fk_Cultivo, setFk_Cultivo] = useState<number | null>(actividad.fk_Cultivo || null);  
+  const [fk_Plantacion, setFk_Plantacion] = useState<number | null>(actividad.fk_Plantacion || null);  
+  const [fecha, setFecha] = useState<string>(actividad.fecha);  
   const [fk_Usuario, setFk_Usuario] = useState<number | null>(actividad.fk_Usuario || null); 
   const [fk_TipoActividad, setFk_TipoActividad] = useState<number | null>(actividad.fk_TipoActividad || null); 
   //manejo mensaje de error
   const [mensajeError, setMensajeError] = useState("")
   //
   const { data: cultivos, isLoading: isLoadingCultivos } = useGetCultivos();
+  const { data: plantaciones, isLoading: isLoadingPlantacion } = useGetPlantaciones();
   const { data: users, isLoading: isLoadingUsers } = useGetUsers();
   const { data: tiposActividad, isLoading: isLoadingTiposActividad } = useGetTipoActividad();
   const { mutate, isPending } = usePatchActividades();  
 
   const handleSubmit = () => {
     // Verificar que todos los campos estén completos
-    if (!fk_Cultivo || !fk_Usuario || !fk_TipoActividad || !titulo || !descripcion || !fecha || !estado) {
+    if (!fk_Usuario || !fk_TipoActividad || !titulo || !descripcion || !fecha || !estado) {
       setMensajeError("Por favor, completa todos los campos.");
 
       return;
+    }
+    if (fk_Cultivo &&  fk_Plantacion) {
+      setMensajeError("No puede elegir cultivo y plantacion al mismo tiempo ")
+      return
     }
     setMensajeError("")
 
@@ -43,16 +51,14 @@ const EditarActividadesModal: React.FC<EditarActividadesModalProps> = ({ activid
           titulo,
           descripcion,
           estado,
-          fk_Cultivo,  
+          fecha,
+          fk_Cultivo,
+          fk_Plantacion,
           fk_Usuario, 
           fk_TipoActividad,
         },
+        onClose,
       },
-      {
-        onSuccess: () => {
-          onClose();  
-        },
-      }
     );
   };
 
@@ -86,6 +92,12 @@ const EditarActividadesModal: React.FC<EditarActividadesModalProps> = ({ activid
         onChange={(e) => setDescripcion(e.target.value)}
         required
       />
+      <Input
+        value={fecha}
+        label="Fecha"
+        onChange={(e) => setFecha(e.target.value)}
+        required
+      />
 
       <Select
         label="Estado"
@@ -115,6 +127,23 @@ const EditarActividadesModal: React.FC<EditarActividadesModalProps> = ({ activid
         >
           {(cultivos || []).map((cultivo) => (
             <SelectItem key={cultivo.id.toString()}>{cultivo.nombre}</SelectItem>
+          ))}
+        </Select>
+      )}
+      {isLoadingPlantacion ? (
+        <p>Cargando plantaciones...</p>
+      ) : (
+        <Select
+          label="Plantacion"
+          placeholder="Selecciona una plantacion"
+          selectedKeys={fk_Plantacion ? [fk_Plantacion.toString()] : []} 
+          onSelectionChange={(keys) => {
+            const selectedKey = Array.from(keys)[0];  
+            setFk_Plantacion(selectedKey ? Number(selectedKey) : null);  
+          }}
+        >
+          {(plantaciones || []).map((p) => (
+            <SelectItem key={p.id.toString()}>{p.cultivo?.nombre}</SelectItem>
           ))}
         </Select>
       )}
