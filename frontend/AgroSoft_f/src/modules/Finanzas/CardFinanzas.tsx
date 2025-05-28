627d72aaa1a20ec96e4952362846d9be1a2c6f60
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-05-28 10:43:12 -0500

    Mejoras en el componente para ttraer mejor informacion

--- frontend/AgroSoft_f/src/modules/Finanzas/CardFinanzas.tsx
+++ frontend/AgroSoft_f/src/modules/Finanzas/CardFinanzas.tsx
@@ -1,23 +1,40 @@
-import { Cosechas } from "./types";
+import { useGetPlantaciones } from "../Trazabilidad/hooks/plantaciones/useGetPlantaciones";
+import { useGetCosechas } from "./hooks/cosechas/useGetCosechas";
 
-type Props = {
-  cosechas: Cosechas[];
-};
+export function CosechasResumenCard() {
+  const { data: cosechas = [], isLoading: loadingCosechas, isError } = useGetCosechas();
+  const { data: plantaciones = [], isLoading: loadingPlantaciones } = useGetPlantaciones();
 
-export function CosechasResumenCard({ cosechas }: Props) {
   const total = cosechas.reduce((acc, c) => acc + c.cantidad, 0);
+
+  if (loadingCosechas || loadingPlantaciones ) return <p>Cargando...</p>;
+  if (isError) return <p>Hubo un error al cargar la información</p>;
 
   return (
     <div className="flex flex-wrap gap-4 mb-6">
-      {cosechas.map((cosecha) => (
-        <div
+      {cosechas.map((cosecha) => {
+        const plantacion = plantaciones.find(p => p.id === cosecha.fk_Plantacion);
+
+        return (
+          <div
           key={cosecha.id}
           className="w-30 h-30 bg-white shadow-md rounded-md border p-2 text-sm flex flex-col justify-center"
-        >
-          <p className="text-gray-500">Fecha Cosecha: {cosecha.fecha}</p>
-          <p className="font-bold">Cantidad: {cosecha.cantidad}</p>
-        </div>
-      ))}
+          >
+          <img
+          src={plantacion?.cultivo.especies.img}
+          alt={plantacion?.cultivo.especies.nombre}
+          className="w-16 h-16 object-cover rounded mx-auto my-2"
+          />
+            <p>
+              <strong>Producto</strong>: {plantacion?.cultivo?.nombre ?? "Desconocido"}
+            </p>
+            <p><strong>Especie</strong>: {plantacion?.cultivo.especies.nombre}</p>
+            <p><strong>Cantidad</strong>: {cosecha.cantidad}</p>
+            <p><strong>Valor cosecha</strong>: {cosecha.valorTotal}</p>
+            <p><strong>Fecha Cosecha</strong>: {cosecha.fecha}</p>
+          </div>
+        );
+      })}
 
       <div className="w-30 h-30 bg-white border border-green-400 rounded-md p-2 text-center flex flex-col justify-center">
         <p className="text-sm font-semibold text-green-800">Cantidad Cosechas</p>
@@ -25,4 +42,33 @@ export function CosechasResumenCard({ cosechas }: Props) {
       </div>
     </div>
   );
+}
+
+export function PlantacionesCard() {
+
+  const { data: plantaciones = [], isLoading: loadingPlantaciones, isError} = useGetPlantaciones();
+
+  if(loadingPlantaciones) return <p>Cargando...</p>
+  if(isError) return <p>Error al cargar...</p>
+
+  return(
+     <div className="flex flex-wrap gap-4 mb-6">
+      {plantaciones.map((plantacion) => {
+
+        return (
+          <div
+          key={plantacion.id}
+          className="w-30 h-30 bg-white shadow-md rounded-md border p-2 text-sm flex flex-col justify-center"
+          >
+            <p>
+              <strong>Cultivo</strong>: {plantacion?.cultivo?.nombre ?? "Desconocido"}
+            </p>
+            <p><strong>Estado</strong>: {plantacion?.cultivo.activo ? "Activo" : "Inactivo"}</p>
+            <p><strong>Fecha de siembra</strong>: {plantacion.fechaSiembra}</p>
+          </div>
+        );
+      })}
+    </div>
+
+  )
 }
