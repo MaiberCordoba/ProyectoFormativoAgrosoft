338f0ee8a7c4078a2ad6fe3f39c9df43161921cd
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-06-05 16:38:56 -0500

    Arreglando conflicto al asignar una actividad

--- frontend/AgroSoft_f/src/modules/Finanzas/components/actividades/CrearActividadModal.tsx
+++ frontend/AgroSoft_f/src/modules/Finanzas/components/actividades/CrearActividadModal.tsx
@@ -100,7 +100,6 @@ export const CrearActividadesModal = ({
           setTitulo("");
           setDescripcion("");
           setFecha("");
-          setMensajeError("");
         },
       }
     );
