de8e3ef882a398d733904f0bb6d5791bd635d72d
Junior-7 <juniorfajardom@gmail.com>
2025-06-05 23:08:34 -0500

    subiendo mensajes para manejar errores en las validaciones del correo en el hook

--- frontend/AgroSoft_f/src/modules/Users/hooks/recuperaciones/useRecuperarContrasena.ts
+++ frontend/AgroSoft_f/src/modules/Users/hooks/recuperaciones/useRecuperarContrasena.ts
@@ -5,10 +5,17 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 export const useRecuperarContrasena = () => {
   return useMutation({
-    mutationFn: async ({email}: { email: string }) => {
-      const { data } = await axios.post(`${apiUrl}solicitar-recuperacion/`, { email });
-      return data;
+    mutationFn: async ({ email }: { email: string }) => {
+      try {
+        const { data } = await axios.post(`${apiUrl}solicitar-recuperacion/`, { email });
+        return data;
+      } catch (error) {
+        // Opcional: lanzar el error con un mensaje legible
+        if (axios.isAxiosError(error) && error.response) {
+          throw new Error(error.response.data.message || "Correo no válido. Por favor verifica e intenta de nuevo.");
+        }
+        throw error;
+      }
     },
   });
 };
-
