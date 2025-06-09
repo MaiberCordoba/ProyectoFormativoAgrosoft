9101d13e74ccd950485fdb899dc47d0e1f312074
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-06-08 21:18:15 -0500

    Arreglando modal de editar uso de herramienta

--- frontend/AgroSoft_f/src/modules/Finanzas/components/usosHerramientas/EditarUsosHerramientasModal.tsx
+++ frontend/AgroSoft_f/src/modules/Finanzas/components/usosHerramientas/EditarUsosHerramientasModal.tsx
@@ -5,6 +5,8 @@ import { UsosHerramientas } from "../../types";
 import { Input, Select, SelectItem } from "@heroui/react";
 import { useGetHerramientas } from "../../hooks/herramientas/useGetHerramientas";
 import { useGetActividades } from "../../hooks/actividades/useGetActividades";
+import { useGetControles } from "@/modules/Sanidad/hooks/controles/useGetControless";
+import { addToast } from "@heroui/toast";
 
 interface EditarUsoHerramientaModalProps {
   usoHerramienta: UsosHerramientas; // El uso de herramienta que se está editando
@@ -14,16 +16,26 @@ interface EditarUsoHerramientaModalProps {
 const EditarUsoHerramientaModal: React.FC<EditarUsoHerramientaModalProps> = ({ usoHerramienta, onClose }) => {
   const [fk_Herramienta, setFk_Herramienta] = useState<number>(usoHerramienta.fk_Herramienta || 0);
   const [fk_Actividad, setFk_Actividad] = useState<number>(usoHerramienta.fk_Actividad || 0);
+  const [fk_Control, setFk_Control] = useState<number>(usoHerramienta.fk_Control || 0);
   const [unidades, setUnidades] = useState(usoHerramienta.unidades);
   const [error, setError] = useState("");
 
   const { data: herramientas, isLoading: isLoadingHerramientas } = useGetHerramientas();
   const { data: actividades, isLoading: isLoadingActividades } = useGetActividades();
+  const { data: controles, isLoading: isLoadingControles } = useGetControles();
   const { mutate, isPending } = usePatchUsosHerramientas();
 
   const handleSubmit = () => {
     if (unidades < 0 ){
       setError("La cantidad no puede ser negativa")
+      return
+    }
+    if (fk_Actividad && fk_Control){
+      addToast({
+        title:"Error",
+        description:"Solo puede relacionar a una actividad o un control, no ambos.",
+        color:"danger"
+      })
       return
     }
     mutate(
@@ -32,6 +44,7 @@ const EditarUsoHerramientaModal: React.FC<EditarUsoHerramientaModalProps> = ({ u
         data: {
           fk_Herramienta,
           fk_Actividad,
+          fk_Control,
           unidades
         },
       },
@@ -81,7 +94,12 @@ const EditarUsoHerramientaModal: React.FC<EditarUsoHerramientaModalProps> = ({ u
           }}
         >
           {(herramientas || []).map((herramienta) => (
-            <SelectItem key={herramienta.id.toString()}>{herramienta.nombre}</SelectItem>
+            <SelectItem key={herramienta.id.toString()} textValue={`Nombre: ${herramienta.nombre} - Disponible: ${herramienta.unidades}`}>
+              <div>
+                <p className="font-medium">{herramienta.nombre}</p>
+                <p className="text-sm text-gray-500">Disponible: {herramienta.unidades}</p>
+              </div>
+            </SelectItem>
           ))}
         </Select>
       )}
@@ -102,6 +120,24 @@ const EditarUsoHerramientaModal: React.FC<EditarUsoHerramientaModalProps> = ({ u
         >
           {(actividades || []).map((actividad) => (
             <SelectItem key={actividad.id.toString()}>{actividad.titulo}</SelectItem>
+          ))}
+        </Select>
+      )}
+      {isLoadingControles ? (
+        <p>Cargando controles...</p>
+      ) : (
+        <Select
+          label="Control"
+          size="sm"
+          placeholder="Selecciona un control"
+          selectedKeys={[fk_Control.toString()]}
+          onSelectionChange={(keys) => {
+            const selectedKey = Array.from(keys)[0];
+            setFk_Control(Number(selectedKey));
+          }}
+        >
+          {(controles || []).map((control) => (
+            <SelectItem key={control.id.toString()}>{control.descripcion}</SelectItem>
           ))}
         </Select>
       )}
